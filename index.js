5291b98e862f290c17be332cfc906cba4e32ddfd
weltonDev <welton.developer95@gmail.com>
2024-03-06 15:23:35 -0300

    teste

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const data = document.querySelector('#datahora');
 const oberservacao = document.querySelector('#observacao');
 const matricula = document.querySelector('#matricula');
 const acessarbtn = document.querySelector('#entrar');
+const alerta = document.querySelector('#alerta');
+const logo = document.querySelector('#logo');
 
-matricula.addEventListener('keyup', (e)=>{
+matricula.addEventListener('keyup', ()=> {
   if(campoMatricula.value.length > 6){
     document.querySelector('.msg').style.display = 'block'
     console.log('Matricula deve ter apenas 6 numeros');
@@ -18,21 +20,54 @@ matricula.addEventListener('keyup', (e)=>{
 
 acessarbtn.addEventListener('click', async (event)=> {
   try {
+    logo.src='./loading.gif'
     event.preventDefault();
-    raw = {
+    if(!matricula.value || !data.value){
+      alerta.innerText = `Preencha todos os campos e tente novamente!`
+      alerta.style.display = 'block'
+      logo.src='./bird.svg'
+      return;
+    }
+
+    const raw = {
       matricula: matricula.value,
       data: data.value,
       oberservacao: oberservacao.value
     }
-    var requestOptions = {
+    const requestOptions = {
       method: 'POST',
       body: JSON.stringify(raw),
-      redirect: 'follow'
+      redirect: 'follow',
+      headers: {
+        "Content-Type": "application/json"
+      }
   };
 
   const resposta = await fetch(`https://conecta.cyclic.app/entradas`, requestOptions)
   const conteudo = await resposta.json();
+
+  if(conteudo == 'Matrícula não cadastrada!'){
+    alerta.innerText = `Matricula não encontrada no banco de dados. Por favor cadastra-se!`;
+    alerta.style.display = 'block'
+    logo.src='./bird.svg'
+    return
+  }
+
+  if(conteudo == 'Matrícula cadastrada com sucesso!'){
+    alerta.innerHTML = `teste`
+    alerta.style.display = 'block'
+    logo.src='./bird.svg'
+    return
+  }
+  
+  // matricula.value = '';
+  // data.value = '';
+  // oberservacao.value = '';
   console.log(conteudo)
+
+  alerta.innerText = conteudo;
+  logo.src='./bird.svg'
+
   } catch (error) {
     console.log(error);
   }
